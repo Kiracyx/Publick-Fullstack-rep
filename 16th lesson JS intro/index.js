3c64c732c7727607c327a1b022800de91fbeec88
Mykola Khomiakov <vkontemail4@gmail.com>
2024-06-08 14:13:42 +0300

    Add 2nd task

--- 16th lesson JS intro/index.js
+++ 16th lesson JS intro/index.js
@@ -6,3 +6,12 @@ if (a > a1) {
 } else {
   console.log("a less than a1");
 }
+
+const b = 2;
+const b1 = "5";
+
+if (b > b1) {
+  console.log("b bigger than b1");
+} else {
+  console.log("b less than b1");
+}
