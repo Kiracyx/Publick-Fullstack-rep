96638e07e3d2a1eba39b2fdadeb2706aa5a09ff6
Mykola Khomiakov <vkontemail4@gmail.com>
2024-06-13 21:40:46 +0300

    Add 3rd task

--- 18th lesson JS Practice Part 1/index.js
+++ 18th lesson JS Practice Part 1/index.js
@@ -17,3 +17,13 @@ function getNum(num) {
 const numRes = getNum(25);
 
 console.log(numRes);
+
+// Task 3
+
+function getSumOfTwoStrings(string1, string2) {
+    return string1 + string2;
+  }
+  
+  const stringRes = getSumOfTwoStrings("I'm going to seek you ", "28");
+  
+  console.log(stringRes);
