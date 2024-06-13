884902b8770b92d0bc8e7b2a188b6f5dd217f901
Mykola Khomiakov <vkontemail4@gmail.com>
2024-06-13 17:51:06 +0300

    Add function avg

--- 17th lesson JS Part 1/index.js
+++ 17th lesson JS Part 1/index.js
@@ -2,5 +2,14 @@ function getRGBValues(ValueR = 0, ValueG = 0, ValueB = 0) {
   const calc = "rgb(" + ValueR + "," + ValueG + "," + ValueB + ")";
   return calc;
 }
-const result = getRGBValues(23, 100, );
+const result = getRGBValues(23, 100, 134);
 console.log(result);
+
+function avg(){
+  console.log(arguments);
+
+  return (num1+num2+num3)/arguments;
+}
+
+const avgRes = avg(5, 3);
+console.log(avgRes);
