d044af762f561b034c1f01f280100d56fa848e7a
Mykola Khomiakov <vkontemail4@gmail.com>
2024-06-13 17:46:03 +0300

    Add function

---
+++ 17th lesson JS Part 1/index.js
@@ -0,0 +1,6 @@
+function getRGBValues(ValueR = 0, ValueG = 0, ValueB = 0) {
+  const calc = "rgb(" + ValueR + "," + ValueG + "," + ValueB + ")";
+  return calc;
+}
+const result = getRGBValues(23, 100, );
+console.log(result);
