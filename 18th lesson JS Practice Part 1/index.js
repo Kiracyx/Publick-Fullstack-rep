b27f8b07290d11acf2e042ac4fe99097d24cf8e9
Mykola Khomiakov <vkontemail4@gmail.com>
2024-06-15 13:46:53 +0300

    Rename task

--- 18th lesson JS Practice Part 1/index.js
+++ 18th lesson JS Practice Part 1/index.js
@@ -1,4 +1,4 @@
-// Task 1
+console.log("Task 1");
 
 function getText(text) {
   return "You wrote " + text;
@@ -8,7 +8,7 @@ const textRes = getText("test");
 
 console.log(textRes);
 
-// Task 2
+console.log("Task 2");
 
 function getNum(num) {
   return 5 + num;
@@ -18,7 +18,7 @@ const numRes = getNum(25);
 
 console.log(numRes);
 
-// Task 3
+console.log("Task 3");
 
 function getSumOfTwoStrings(string1, string2) {
   return string1 + string2;
@@ -28,7 +28,7 @@ const stringRes = getSumOfTwoStrings("I'm going to seek you ", "28");
 
 console.log(stringRes);
 
-// Task 4
+console.log("Task 4");
 
 function getProdOfTwo(num1, num2) {
   return num1 * num2;
@@ -38,7 +38,7 @@ const numOfTwoRes = getProdOfTwo(25, 2);
 
 console.log(numOfTwoRes);
 
-// Task 5
+console.log("Task 5");
 
 function getDivOfTwo(num1, num2) {
   const sumOfTwoDiv = num1 % num2;
@@ -54,7 +54,7 @@ function getDivOfTwo(num1, num2) {
 
 getDivOfTwo(25, 25);
 
-// Task 6
+console.log("Task 6");
 
 function getPowOfTwo(num1, num2) {
   return Math.pow(num1, num2);
