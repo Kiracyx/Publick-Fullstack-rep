0bdd506ed2f56c992ba011fce5eb6c45c2f0361c
Mykola Khomiakov <vkontemail4@gmail.com>
2024-06-21 15:18:30 +0300

    Add tasks

---
+++ 19th lesson JS Objects and Comp Props/objects.js
@@ -0,0 +1,77 @@
+const user = {
+  firstname: "Jhon",
+  age: 32,
+};
+
+console.log(user);
+
+const cat = {
+  tailLength: 20,
+  sayMeow: function () {
+    return "Meow";
+  },
+};
+
+console.log(cat, cat.sayMeow());
+
+const Jiraph = {
+  age: 25,
+  weight: 235,
+  asleep: true,
+  bodyHeight: 3.5,
+  neckLength: 3,
+  stature: function () {
+    if (
+      typeof this.bodyHeight !== "number" ||
+      typeof this.neckLength !== "number"
+    ) {
+      return null;
+    }
+    if (isNaN(this.bodyHeight) || isNaN(this.neckLength)) {
+      return null;
+    }
+    const sum = this.bodyHeight + this.neckLength;
+    return `Jiraph stature is ` + Number(sum);
+  },
+  waky: function () {
+    if (this.asleep === true) {
+      return (this.asleep = false);
+    }
+  },
+};
+
+console.log(Jiraph.stature());
+
+Jiraph.waky();
+
+console.log(Jiraph.asleep);
+
+class motorcycle {
+  brand = null;
+  tankCapacity = null;
+  engineModel = null;
+  seats = null;
+  currentSpeed = 100;
+  constructor(brand1, tankCapacity1, engineModel1, seats1) {
+    this.brand = brand1;
+    this.tankCapacity = tankCapacity1;
+    this.engineModel = engineModel1;
+    this.seats = seats1;
+  }
+
+  increaseSpeed() {
+    return (this.currentSpeed += 55);
+  }
+  decreaseSpeed() {
+    return (this.currentSpeed -= 20);
+  }
+  stopTheVehicle() {
+    return (this.currentSpeed = 0);
+  }
+}
+
+const HYZ = new motorcycle("GeonScrumbler", 12, "asda212", 2);
+
+console.log(HYZ);
+HYZ.increaseSpeed();
+console.log(HYZ);
