2b2fa68f6174740d069e934dfea9dbfcf59e3530
Mykola Khomiakov <vkontemail4@gmail.com>
2024-07-03 00:23:48 +0300

    Add find examples

--- 21th lesson DOM intro/assets/index.js
+++ 21th lesson DOM intro/assets/index.js
@@ -3,4 +3,35 @@
     Choose - textContent
 */
 
-document.body.textContent = "test";
+// document.body.textContent = "test";
+
+/* 
+innerHTML
+ */
+
+// document.body.innerHTML = "<div>second test</div>";
+
+/* 
+getElementById()
+ */
+
+// const elem = document.getElementById("first");
+// console.dir(elem)
+// elem.innerHTML = "<input>";
+
+/* 
+getElementsByClassName()
+ */
+
+// const elem = document.getElementsByClassName("second");
+
+// console.log(elem);
+
+// const array = Array.from(elem);
+// console.log(array);
+
+/* 
+querySelectorAll()
+ */
+const elem = document.querySelectorAll(".second");
+
