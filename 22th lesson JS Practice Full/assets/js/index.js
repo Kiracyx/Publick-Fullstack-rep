ade7d8c9ccb6d311a6c00b42bd12afc75a3511d1
Mykola Khomiakov <vkontemail4@gmail.com>
2024-07-07 15:24:24 +0300

    Add class axamples

--- 22th lesson JS Practice Full/assets/js/index.js
+++ 22th lesson JS Practice Full/assets/js/index.js
@@ -1,24 +1,62 @@
-class User {
-  constructor(email = null, gender = null, phone = null, imgSrc = null) {
-    this.email = email;
-    this.gender = gender;
-    this.phone = phone;
-    this.imgSrc = imgSrc;
-    this.score = 0;
+class University {
+  constructor(name = null, city = null, yearOfFounding = null, faculties) {
+    this.name = name;
+    this.city = city;
+    this.yearOfFounding = yearOfFounding;
+    this.faculties = faculties;
   }
 
-  sendSms(message) {
-    console.log(`SMS sent to ${this.phone} with message: ${message}`);
+  addFaculty(facultyName) {
+    this.faculties.push(facultyName);
+    return this.faculties;
   }
-  sendEmail(message) {
-    `Email sent to ${this.email} with message: ${message}`;
-  }
-  addScore(amount) {
-    this.score += amount;
-    return this.score;
+  registerStudent(studentInstance, facultyName) {
+    if (!this.faculties.includes(facultyName)) {
+      throw new Error(
+        "faculties array doesn't includ facultyName: " + facultyName
+      );
+    }
+    if (!studentInstance instanceof Student) {
+      throw new Error("studentInstance is not an instanceof Student");
+    }
+
+    studentInstance.yearOfRegistry = new Date().getFullYear();
+    studentInstance.facultyName = facultyName;
+    studentInstance.universityName = this.name;
+    return studentInstance;
   }
 }
 
-const user1 = new User();
+class Student {
+  constructor(fullName = null, age = null, examScore = null) {
+    this.fullName = fullName;
+    this.age = age;
+    this.examScore = examScore;
+    this.yearOfRegistry = null;
+    this.universityName = null;
+    this.facultyName = null;
+  }
 
-console.log(user1);
+  isGraduated() {
+    let result = new Date().getFullYear() - this.yearOfRegistry;
+    if (result < 5) {
+      throw new Error("Is not graduated yet!");
+    }
+  }
+}
+
+const kpi = new University("Kiev Polithechnical Institute", "Kiev", 1937, [
+  "Faculty of Engineering",
+  "Faculty of Physics",
+  "Faculty of Economics",
+]);
+
+const student1 = new Student("John Doe", 19, 140);
+
+console.log(kpi);
+
+console.log(student1);
+
+const regResult = kpi.registerStudent(student1, "Faculty of Engineering");
+
+console.log(regResult);
