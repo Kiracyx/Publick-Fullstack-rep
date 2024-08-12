2bf7081713466c9ec50663c768535a85b99b2fe6
Mykola Khomiakov <vkontemail4@gmail.com>
2024-08-12 20:35:39 +0300

    Add 31th lesson Node.js intro folder

---
+++ 31th_lesson_Node.js_intro/assets/js/MyMath.js
@@ -0,0 +1,8 @@
+class MyMath {
+  constructor() {
+    this.sum = (a, b) => a + b;
+  }
+  mult = (a, b) => a * b;
+}
+
+module.exports = MyMath;
---
+++ 31th_lesson_Node.js_intro/assets/js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const MyMath = require("./MyMath");
+
+const math = new MyMath();
+
+console.log(math);
+
+console.log(math.sum(2, 2));
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(Math.random());
+// }
