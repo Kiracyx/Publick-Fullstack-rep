9a77bf01a61ceea419833ae8ee3b9a23791aefc6
Mykola Khomiakov <vkontemail4@gmail.com>
2024-08-13 21:12:10 +0300

    Add helloFile.txt to 31th lesson

--- 31th_lesson_Node.js_intro/assets/js/fsinit.js
+++ 31th_lesson_Node.js_intro/assets/js/fsinit.js
@@ -1,18 +1,38 @@
 const fs = require("fs/promises");
+const { encode } = require("querystring");
 
-fs.mkdir("/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/tmp").then((err) => {
-  console.log("dir successfully");
-});
+// fs.mkdir(
+//   "/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/tmp"
+// ).then((err) => {
+//   console.log("dir successfully");
+// });
 
-fs.writeFile(
-  "/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/tmp/info.txt",
-  "'Hello world and Nodejs'"
-).then((err) => {
-  console.log("file successfully");
-});
+// fs.writeFile(
+//   "/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/tmp/info.txt",
+//   "'Hello world and Nodejs'"
+// ).then((err) => {
+//   console.log("file successfully");
+// });
+
+// fs.readFile(
+//   "/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/Readme.md",
+//   { encoding: "utf8" }
+// ).then((file) => {
+//   console.log(file);
+// });
+
+        "First task"
+
+// fs.writeFile(
+//   "/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/tmp/helloFile.txt",
+//   "'Hello from NodeJS'"
+// ).then((err) => {
+//   console.log("file successfully");
+// });
 
 fs.readFile(
-  "/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/Readme.md"
+  "/Users/vkont/OneDrive/Рабочий стол/Learning/JS-Fullstack-repository/31th_lesson_Node.js_intro/tmp/helloFile.txt",
+  { encoding: "utf8" }
 ).then((file) => {
-  console.log("read successfully");
+  console.log(file);
 });
