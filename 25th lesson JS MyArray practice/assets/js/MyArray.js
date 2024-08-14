4f25b5fb0b433f06ae8fc97fa9c4a80a0b5f42d0
Mykola Khomiakov <vkontemail4@gmail.com>
2024-08-14 18:31:50 +0300

    Add incoplete tasks

--- 25th lesson JS MyArray practice/assets/js/MyArray.js
+++ 25th lesson JS MyArray practice/assets/js/MyArray.js
@@ -1,13 +1,26 @@
 class MyArray {
-  constructor() {
-    for (let i = 0; i < arguments.length; i++) {
-      this[i] = arguments[i];
+  constructor(...rest) {
+    for (let i = 0; i < rest.length; i++) {
+      this[i] = rest[i];
     }
-    this.length = arguments.length;
+    this.length = rest.length;
+  }
+
+  push(...rest) {
+    for (let i = 0; i < rest.length; i++) {
+      this[i + this.length] = rest[i];
+    }
+    this.length += rest.length;
+
+    return this.length;
   }
 
   shift() {
     const first = this[0];
+
+    if (this.length === 0) {
+      return undefined;
+    }
 
     for (let i = 0; i < this.length; i++) {
       this[i] = this[i + 1];
@@ -19,18 +32,14 @@ class MyArray {
     return first;
   }
 
-  unshift() {
-    const first = this[0];
+  // unshift(...rest) {
+  //   for (let i = 0; i < this.length; i++) {
+  //     this[i] = value;
+  //     this[i + 1] = this[i];
+  //   }
 
-    for (let i = 0; i < this.length; i++) {
-      this[i + 1] = this[i];
-    }
-
-    delete this[this.length - 1];
-    this.length -= 1;
-
-    return first;
-  }
+  //   return (this.length += 1);
+  // }
 
   forEach(callbackfn) {
     if (typeof callbackfn !== "function") {
@@ -49,16 +58,41 @@ class MyArray {
       throw new TypeError(callbackfn + " is not a function");
     }
 
-    const newArray = {};
+    const newArray = new MyArray();
 
     for (let i = 0; i < this.length; i++) {
       const element = this[i];
 
       const result = callbackfn(element, i, this);
 
-      newArray[i] = result;
+      newArray.push(result);
     }
 
     return newArray;
   }
+
+  findIndex(callbackfn) {
+    if (typeof callbackfn !== "function") {
+      throw new TypeError(callbackfn + " is not a function");
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+
+      const result = callbackfn(element, i, this);
+
+      if (Boolean(result)) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
+
+const arr = new MyArray(1, 2, 3, 4, 5);
+
+// console.log(
+//   arr.findIndex((element) => {
+//     return element % 2 === 0;
+//   })
+// );
